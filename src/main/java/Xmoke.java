85db9dd1a0e6dba8408958e2e909283deb5f2b83
XMOKE666 <wwwniexmcom@gmail.com>
2026-02-02 00:55:14 +0800

    Level-2: Add, List

--- src/main/java/Xmoke.java
+++ src/main/java/Xmoke.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class Xmoke {
-    private static final String LINE_SEPARATOR =
-            "____________________________________________________________";
+    private static final String LINE_SEPARATOR = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
         System.out.println(LINE_SEPARATOR);
         System.out.println("Hello! I'm XMOKE");
         System.out.println("What can I do for you?");
@@ -19,8 +22,22 @@ public class Xmoke {
                 System.out.println(LINE_SEPARATOR);
                 break;
             }
+            if (input.equals("list")) {
+                System.out.println(LINE_SEPARATOR);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(LINE_SEPARATOR);
+                continue;
+            }
+            if (taskCount >= MAX_TASKS) {
+                System.out.println("I can't take it anymore!");
+                continue;
+            }
+            tasks[taskCount] = input;
+            taskCount++;
             System.out.println(LINE_SEPARATOR);
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println(LINE_SEPARATOR);
         }
         scanner.close();
