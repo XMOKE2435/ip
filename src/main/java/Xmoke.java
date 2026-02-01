7772b8df5f3df8d7ad0c883db138b6e3501d7df4
XMOKE666 <wwwniexmcom@gmail.com>
2026-02-01 22:17:31 +0800

    Level-1: Echo

--- src/main/java/Xmoke.java
+++ src/main/java/Xmoke.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Xmoke {
+    private static final String LINE_SEPARATOR =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(LINE_SEPARATOR);
         System.out.println("Hello! I'm XMOKE");
         System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE_SEPARATOR);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.trim().equalsIgnoreCase("bye")) {
+                System.out.println(LINE_SEPARATOR);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE_SEPARATOR);
+                break;
+            }
+            System.out.println(LINE_SEPARATOR);
+            System.out.println(input);
+            System.out.println(LINE_SEPARATOR);
+        }
+        scanner.close();
     }
 }
